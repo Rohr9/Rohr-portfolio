573d370728b6aa627115d4df6241b7f737ad4cbf
Marcus <marcusslotrohr@gmail.com>
2024-03-05 10:24:48 +0100

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    var image = document.getElementById('contactImage');
+    var message = document.querySelector('.middle .textipic'); // Select the .textipic within .middle
+
+    // Hover event
+    image.addEventListener("mouseover", function() {
+        console.log("Mouse over image");
+        message.style.display = 'block';
+    });
+
+    image.addEventListener("mouseout", function() {
+        console.log("Mouse out image");
+        message.style.display = 'none';
+    });
+
+    // Click event
+    message.addEventListener("click", function() {
+        console.log("Clicked!"); // Log to console to verify if the click event works
+        window.location.href = "page3.html"; // Redirect to contact page
+    });
+});
+*/
